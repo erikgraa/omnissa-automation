824632a9e5f7286b6f34156cd574557d5353865b
Erik Grina Raassum <20067955+erikgraa@users.noreply.github.com>
2025-03-25 01:41:42 +0100

    Added Omnissa port list cmdlet and workflow

---
+++ misc/powershell/Get-OmnissaPortList.ps1
@@ -0,0 +1,41 @@
+﻿  <#
+    .DESCRIPTION
+    Retrieves Omnissa port list.
+
+    .EXAMPLE
+    Get-OmnissaPortList
+
+    .OUTPUTS
+    PSCustomObject.
+
+    .LINK
+    https://ports.omnissa.com/
+#>
+
+function Get-OmnissaPortList {
+  [CmdletBinding()]
+  [OutputType([HashTable])]
+  param ()
+
+  begin {
+    $baseUri = 'https://ports-manage-svc.gid.omnissa.com/manage/view/v1/omnissaproducts'
+
+    $products = Invoke-RestMethod -Uri $baseUri -Method Get
+
+    $hash = [Ordered]@{}    
+  }
+
+  process {
+    foreach ($_product in ($products | Sort-Object -Property productName)) {
+      $ports = Invoke-RestMethod -Uri ('{0}/{1}/listings1' -f $baseUri, $_product.id) -Method Get
+
+      if (($ports | Measure-Object).Count -ne 0) {
+        $hash.Add($_product.productName, $ports)
+      }
+    }
+  }
+
+  end {
+    $hash
+  }
+}
